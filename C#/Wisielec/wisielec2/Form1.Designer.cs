6c4dae760280d8994152e352daf0b2decdd8d84d
mwojtkowiak <49155326+mwojtkowiak98@users.noreply.github.com>
2020-05-18 15:28:19 +0200

    Add files via upload

---
+++ C#/Wisielec/wisielec2/Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace wisielec2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.Menu;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(340, 287);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(383, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 54);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Losuj haslo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(383, 233);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(104, 66);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Zatwierdz";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(383, 197);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(103, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 321);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "WYLOSOWANE HASLO";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 348);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 379);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(139, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "WYKORZYSTANE LITERY";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 406);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label5.Location = new System.Drawing.Point(114, 221);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(21, 31);
+            this.label5.TabIndex = 8;
+            this.label5.Text = " ";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label6.Location = new System.Drawing.Point(113, 183);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(21, 31);
+            this.label6.TabIndex = 9;
+            this.label6.Text = " ";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label7.Location = new System.Drawing.Point(114, 146);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(21, 31);
+            this.label7.TabIndex = 10;
+            this.label7.Text = " ";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label8.Location = new System.Drawing.Point(114, 109);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(21, 31);
+            this.label8.TabIndex = 11;
+            this.label8.Text = " ";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label9.Location = new System.Drawing.Point(146, 91);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(21, 31);
+            this.label9.TabIndex = 12;
+            this.label9.Text = " ";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label10.Location = new System.Drawing.Point(170, 112);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(21, 31);
+            this.label10.TabIndex = 13;
+            this.label10.Text = " ";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label11.Location = new System.Drawing.Point(170, 139);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(21, 31);
+            this.label11.TabIndex = 14;
+            this.label11.Text = " ";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label12.Location = new System.Drawing.Point(170, 165);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(21, 31);
+            this.label12.TabIndex = 15;
+            this.label12.Text = " ";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label13.Location = new System.Drawing.Point(170, 187);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(21, 31);
+            this.label13.TabIndex = 16;
+            this.label13.Text = " ";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(386, 167);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(116, 13);
+            this.label14.TabIndex = 17;
+            this.label14.Text = "TYLKO DUZE LITERY";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(519, 436);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+    }
+}
+
---
+++ C#/Wisielec/wisielec2/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace wisielec2
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string[] slowa = {
+            "ANAKONDA",
+            "FIBONACCI",
+            "OKON"
+        };
+        string podana, haslo;
+        int i, ilosc, los;
+        static char[] tab;
+        int blad = 0;
+
+        private void button1_Click(object sender, EventArgs e) //losuj
+        {
+            label2.Text = "";
+            los = random.Next(0, 3);
+            haslo = slowa[los];
+
+            ilosc = haslo.Length;
+            tab = new char[ilosc];
+
+            for (i = 0; i < ilosc; i++)
+            {
+                tab[i] = '_';
+            }
+
+            for (i = 0; i < ilosc; i++)
+            {
+                label2.Text += " " + tab[i] + " ";
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e) //sprawdz
+        {
+            
+            podana = textBox1.Text;
+            label2.Text = "";
+            textBox1.Text = "";
+
+            for (i=0; i<ilosc; i++)
+            {
+                if (podana[0] == haslo[i])
+                {
+                    tab[i] = podana[0];
+                }
+                if (podana[0] != haslo[i])
+                {
+                    blad++;
+                }
+            }
+            blad = blad - ilosc + 1;
+
+
+            for (i = 0; i < ilosc; i++)
+            {
+                label2.Text += " " + tab[i] + " ";
+            }
+
+
+            if (blad==1)
+            {
+                label5.Text = "//\\";
+            }
+            if (blad == 2)
+            {
+                label6.Text = " || ";
+            }
+            if (blad == 3)
+            {
+                label7.Text = " || ";
+            }
+            if (blad == 4)
+            {
+                label8.Text = "  //";
+            }
+            if (blad == 5)
+            {
+                label9.Text = "==";
+            }
+            if (blad == 6)
+            {
+                label10.Text = " | ";
+            }
+            if (blad == 7)
+            {
+                label11.Text = " O ";
+            }
+            if (blad == 8)
+            {
+                label12.Text = " /|/";
+            }
+            if (blad == 9)
+            {
+                label13.Text = " //";
+            }
+            if (blad == 10)
+            {
+                richTextBox1.Text = "PRZEGRALES";
+            }
+
+            label4.Text += podana[0] + " ";
+
+        }
+
+        private void label5_Click(object sender, EventArgs e) //5-12
+        {
+
+        }
+
+        Random random = new Random();
+
+    }
+}
